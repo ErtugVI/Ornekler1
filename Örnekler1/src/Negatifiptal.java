5d52200b64950f6d183ae89b8aa8c5df38862acb
Arda Coskun <82402188+ArdaCoskunnn@users.noreply.github.com>
2021-04-14 19:05:42 +0300

    Update Negatifiptal.java
    lan şu kodları düzeni yaz amk kör oldum

--- Örnekler1/src/Negatifiptal.java
+++ Örnekler1/src/Negatifiptal.java
@@ -3,20 +3,24 @@ import java.util.Scanner;
 public class Negatifiptal {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-	 Scanner scan = new Scanner(System.in);
-	 int teksayilarintoplami = 0;
-	 int girilensayi;
-	 while(true){
-	System.out.println("Sayý giriniz.");
-	girilensayi = scan.nextInt();
-	if(girilensayi<0) { System.out.println("Negatif bir sayý girdiniz. Ýþleminiz sonlandýrýldý.");
+
+		Scanner scan = new Scanner(System.in);
+		int teksayilarintoplami = 0, girilensayi;
+		
+		while(true){	
+			System.out.println("Sayi giriniz.");
+			girilensayi = scan.nextInt();
+		
+			if(girilensayi<0) { 
+				System.out.println("Negatif bir sayi girdiniz. Isleminiz sonlandirildi.");
 		    	
-	System.out.println("Tek sayýlarýn toplamý ;" + teksayilarintoplami);
-	 break;} 
-	 if(girilensayi%2==1) {
-	teksayilarintoplami+=girilensayi;}
-	 }
+				System.out.println("Tek sayilarin toplami ;" + teksayilarintoplami);
+	 			break;
+			} 
+			if(girilensayi%2==1) {
+				teksayilarintoplami+=girilensayi;
+			}
+		}
 	}
 }
 	 
